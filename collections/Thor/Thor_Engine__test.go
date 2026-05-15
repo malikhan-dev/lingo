9a13f9bad94728ce4a53b4cf275eb6c12e09e79a
mohammadreza malikhan <109470179+malikhan-dev@users.noreply.github.com>
2026-05-16 00:09:51 +0330

    Update Thor_Engine__test.go

--- collections/Thor/Thor_Engine__test.go
+++ collections/Thor/Thor_Engine__test.go
@@ -33,11 +33,11 @@ func init() {
 
 func TestQueryEngine(t *testing.T) {
 
-	result := from(items).where(func(search ComplexObjectToSearch) bool {
+	result := From(items).Where(func(search ComplexObjectToSearch) bool {
 		return search.Name == "Jane" && search.Flag == false
 	}).Collect()
 
-	result2 := from(result).any(func(search ComplexObjectToSearch) bool {
+	result2 := From(result).Any(func(search ComplexObjectToSearch) bool {
 		return (search.Name != "Jane") || (search.Flag != false)
 	}).Assert()
 
@@ -49,41 +49,24 @@ func TestQueryEngine(t *testing.T) {
 
 func TestGroupByNew(t *testing.T) {
 
-	/*	ctx, _ := context.WithCancel(context.Background())
-	 */
-	res := Collect(Group[bool, ComplexObjectToSearch](from(items).where(func(search ComplexObjectToSearch) bool {
+	res :=
+		Collect(
+			Group[bool, ComplexObjectToSearch](
 
-		return search.Age > 50000
+				From(items).Where(func(search ComplexObjectToSearch) bool {
 
-	}), "Flag"))
+					return search.Age > 20
+
+				}),
+				func(item ComplexObjectToSearch) bool {
+					return item.Flag
+				}))
 
 	fmt.Println(res.Items[false][1])
 	fmt.Println(res.Items[true][1])
 
-	/*for _, v := range res.Items[false] {
-		fmt.Println(v)
-	}*/
 	fmt.Println("==========================================================")
 	fmt.Println("==========================================================")
 	fmt.Println("==========================================================")
-	/*for _, v := range res.Items[true] {
-		fmt.Println(v)
-	}*/
-
-	/*fmt.Println(len(res.Items[true]))*/
-	/*
-
-
-		res2 := res.Items[true]
-
-		res3 := res.Items[false]
-
-		for i := range streams.FromData(ctx, 256, res2) {
-			fmt.Println(i)
-		}
-
-		for i := range streams.FromData(ctx, 256, res3) {
-			fmt.Println(i)
-		}*/
 
 }
